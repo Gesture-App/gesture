19797a76b6efe1922e7c9bdd010c4124aeb766f7
Jacky Zhao <j.zhao2k19@gmail.com>
2022-08-22 10:56:24 -0400

    base ios, folder structure

---
+++ ios/Gesture/Gesture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gesture
+//
+//  Created by Jacky Zhao on 2022-08-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/Gesture/Gesture/GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureApp.swift
+//  Gesture
+//
+//  Created by Jacky Zhao on 2022-08-22.
+//
+
+import SwiftUI
+
+@main
+struct GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
